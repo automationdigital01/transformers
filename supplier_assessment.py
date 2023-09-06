12fec794e20a74b3b15beb03a68845840eb2dbcd
automationdigital01 <123158399+automationdigital01@users.noreply.github.com>
2023-09-06 16:13:36 +0530

    Update supplier_assessment.py

--- supplier_assessment.py
+++ supplier_assessment.py
@@ -14,11 +14,12 @@ def web_scraping(URL):
     headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"}
     # Here the user agent is for Edge browser on windows 10. You can find your browser user agent from the above given link.
     #URL="https://www.investorsobserver.com/news/stock-update/is-halliburton-company-hal-the-right-choice-in-oil-gas-equipment-services"
-    r = requests.get(url=URL,verify=False, headers=headers)
+    #r = requests.get(url=URL,verify=False, headers=headers)
+    r = requests.get(url=URL, headers=headers)
     soup = BeautifulSoup(r.text, "html.parser")
-    st.write(soup)
     # Get the whole body tag
     tag = soup.body
+    #article_body = soup.find("div", class_="article__body-content")
     full_text=""
     # Print each string recursively
     for string in tag.strings:
