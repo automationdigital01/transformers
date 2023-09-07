82abb65bfceb971262134eff6b67857177236bcd
automationdigital01 <123158399+automationdigital01@users.noreply.github.com>
2023-09-07 12:49:27 +0530

    Update supplier_assessment.py

--- supplier_assessment.py
+++ supplier_assessment.py
@@ -14,7 +14,7 @@ def web_scraping(URL):
     headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"}
     # Here the user agent is for Edge browser on windows 10. You can find your browser user agent from the above given link.
     #URL="https://www.investorsobserver.com/news/stock-update/is-halliburton-company-hal-the-right-choice-in-oil-gas-equipment-services"
-    r = requests.get(url=URL,verify=True, headers=headers)
+    r = requests.get(url=URL,verify=False, headers=headers)
     
     soup = BeautifulSoup(r.text, "html.parser")
     # Get the whole body tag
