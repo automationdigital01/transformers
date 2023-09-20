36301e7b868ccd83765dcdba6edfd8a37882b47f
automationdigital01 <123158399+automationdigital01@users.noreply.github.com>
2023-09-20 17:07:35 +0530

    Update supplier_assessment.py

--- supplier_assessment.py
+++ supplier_assessment.py
@@ -197,7 +197,7 @@ def main():
                 
                 #st.write(text)
                 summary=summarize(text)
-                #st.write("Summary:",summary)
+                st.write("Summary:",summary)
                 sentiment=sent_analysis(summary)
                 st.write("Analysis:", sentiment)                
                             
