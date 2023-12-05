380771b085919ebc88cdd6ec0a43229ff801f13f
automationdigital01 <123158399+automationdigital01@users.noreply.github.com>
2023-12-05 14:33:42 +0530

    Update legal_doc.py

--- legal_doc.py
+++ legal_doc.py
@@ -1,9 +1,10 @@
 import streamlit as st
+from annotated_text import annotated_text
 from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 #read pdf
 import PyPDF2
-txt = st.text_area("")
+uploaded_file = st.file_uploader("Choose a file")
 
 def read_pdf(file):
     pdf_reader = PyPDF2.PdfReader(file)
@@ -13,13 +14,16 @@ def read_pdf(file):
         text += page.extract_text()
     return text
 
-#file="./48343-2023-Technip Energies France SAS- SAPURA-13042023.pdf"
-#text=read_pdf(file)
-#print(text)
-tokenizer = AutoTokenizer.from_pretrained("harshildarji/gbert-legal-ner", use_auth_token="AUTH_TOKEN")
-model = AutoModelForTokenClassification.from_pretrained("harshildarji/gbert-legal-ner", use_auth_token="AUTH_TOKEN")
-
-ner = pipeline("ner", model=model, tokenizer=tokenizer)
-example = txt
-results = ner(example)
-st.write(results)
+if uploaded_file and st.button("submit"):
+    
+    #file="./48343-2023-Technip Energies France SAS- SAPURA-13042023.pdf"
+    text=read_pdf(uploaded_file)
+    #print(text)
+    tokenizer = AutoTokenizer.from_pretrained("harshildarji/gbert-legal-ner", use_auth_token="AUTH_TOKEN")
+    model = AutoModelForTokenClassification.from_pretrained("harshildarji/gbert-legal-ner", use_auth_token="AUTH_TOKEN")
+    
+    ner = pipeline("ner", model=model, tokenizer=tokenizer)
+    example = txt
+    results = ner(example)
+    st.write(results)
+    annotated_text(results)
