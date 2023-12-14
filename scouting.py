08b488c5ffffc71c1ea6e89af8ddb5ef2b086852
automationdigital01 <123158399+automationdigital01@users.noreply.github.com>
2023-12-14 13:53:19 +0530

    Update scouting.py

--- scouting.py
+++ scouting.py
@@ -209,10 +209,10 @@ def main():
         for URL in valid_urls:
             r = requests.get(url=URL,verify=False, headers=headers)
             soup = BeautifulSoup(r.text, "html.parser")
-            title=soup.find('title')
-            st.write(title)
+            #title=soup.find('title')
+            #st.write(title)
             text = soup.get_text()
-            if words_in_string(keywords_to_search, text) or words_in_string(keywords_to_search, title) or words_in_string(morekeywords_to_search, text) or words_in_string(morekeywords_to_search, title) :
+            if words_in_string(keywords_to_search, text) or words_in_string(morekeywords_to_search, text):
                 st.write(URL)
                 st.write('One or more words found!')
                 #descriptions=summary(text)
